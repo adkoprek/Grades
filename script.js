f8b76662207a1944f368807deff2c243785ad1ba
Adam Koprek <adam.koprek@halfooda.com>
2024-03-04 19:35:39 +0100

    Initial Commito

---
+++ script.js
@@ -0,0 +1,84 @@
+function change_value(id, value) {
+    let input = document.getElementById(id);
+    if (input.value == "") input.value = 0.5;
+    let new_value = parseFloat(input.value) + value;
+    if (new_value < 1 || new_value > 6) return;
+    input.value = new_value;
+}
+
+function calculate_average() {
+    let inputs = [
+         document.getElementById("dt"), 
+         document.getElementById("ma"), 
+         document.getElementById("nt"),
+         document.getElementById("rz"), 
+         document.getElementById("en"),
+         document.getElementById("fz"),
+         document.getElementById("pb"),
+         document.getElementById("wh"),
+         document.getElementById("mi"),
+         document.getElementById("mu"),
+         document.getElementById("bs"), 
+         document.getElementById("wp") 
+    ];
+
+    let values = {
+        "dt": 0,
+        "ma": 0,
+        "nt": 0,
+        "rz": 0,
+        "en": 0,
+        "fz": 0,
+        "pb": 0,
+        "wh": 0,
+        "mi": 0,
+        "mu": 0,
+        "bs": 0,
+        "wp": 0
+    };
+
+    let error_detected = false;
+
+    inputs.forEach(input => {
+        input.classList.remove("error");
+        if (input.value == "" && input.id != "wp") {
+            input.classList.add("error");
+            error_detected = true;
+        }
+        else if (input.id != "wp") {
+            let value = parseFloat(input.value);
+            
+            if (value < 1 || value > 6 || value % 0.5 != 0) {
+                input.classList.add("error");
+                error_detected = true;
+            }
+            else values[input.id] = value; 
+        }
+    });
+
+    if (error_detected) return;
+
+
+    let special_group_pwm = (values["pb"] + values["wh"] + values["mi"]) / 3;
+    let special_group_mbw;
+    if (values["wp"] == 0) special_group_mbw = (values["mu"] + values["bs"]) / 2;
+    else special_group_mbw = (values["mu"] + values["bs"] + values["wp"]) / 3;
+
+    special_group_pwm = Math.round(special_group_pwm * 10) / 10;
+    special_group_mbw = Math.round(special_group_mbw * 10) / 10;
+
+    console.log(special_group_pwm);
+    console.log(special_group_mbw);
+
+    let summ = values["dt"] * 2 + values["ma"] * 2 + values["nt"] * 2 + values["rz"] * 2 +
+                values["en"] + values["fz"] + special_group_pwm + special_group_mbw;
+                
+    let durchschnitt = summ / 12;
+
+    document.getElementById('solution').innerText = 'Durchschnittsnote: ' + durchschnitt.toFixed(1);
+}
+
+function empty_form() {
+    let form = document.getElementById("grade-form");
+    form.reset();
+}
